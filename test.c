ca7d4e6758b2ef1517a6c3426d6fb38bb52619a5
andrew841018 <andrew841018@gmail.com>
2021-12-28 18:09:05 +0800

    add original one

---
+++ test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct _a{
+	int l;
+}a;
+int main(){
+	char *substr=NULL,buffer[1024];
+	const char *const delim=" ";
+	int sector_number,i=55,k=22;
+	char a[1000][100]={0},b[100];
+	fclose(i);
+	for(i=0;i<40;i++)
+		b[100]=5;
+/*	FILE *a=fopen("run1_Postmark_2475.txt","r");	
+	while (fgets(buffer,1024,a)!=NULL){
+		substr=(char*)strtok(buffer,delim);
+		substr=(char*)strtok(NULL,delim);//second num
+        	substr=(char*)strtok(NULL,delim);//third...sector_num
+//        	sector_number=atoi(substr);*/
+//}
+	return 0;
+}
