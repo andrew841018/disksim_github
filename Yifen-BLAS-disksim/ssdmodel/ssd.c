70a566d619225a56ab2e97d95f2b8b51dc23eb2b
andrew <andrew841018@gmail.com>
2022-01-09 17:54:09 +0800

    1754

--- Yifen-BLAS-disksim/ssdmodel/ssd.c
+++ Yifen-BLAS-disksim/ssdmodel/ssd.c
@@ -11,7 +11,7 @@
 #include "modules/ssdmodel_ssd_param.h"
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
+#include <assert.h> 
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/time.h>
