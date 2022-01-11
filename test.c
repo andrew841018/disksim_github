5746cd6e7cc29555964d80578cc746e9df318298
andrew841018 <andrew841018@gmail.com>
2022-01-11 15:49:38 +0800

    complete disksim

--- test.c
+++ test.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-typedef struct write_buffer{
-	struct write_buffer *a[10000];
-	struct write_buffer *b[10000];
-	int p;
-}buf;
-buf *test(buf *wb){
-	wb=malloc(sizeof(buf));
-	wb->a[0]=malloc(sizeof(buf));
-	return wb;
-}
 int main(){
 	int i;
+	
+	char tmp[100];
 	for(i=0;i<10;i++){
-		buf *wb;
-		wb=test(wb);
-		printf("wb:%p a[0]:%p\n",wb,wb->a[0]);	
-}
+		FILE *a=fopen("a.txt","w+");
+		sprintf(tmp,"%d",i);
+		fprintf(a,"%s\n",tmp);
+		fclose(a);
+	}
+	
 	/*
 	float benefit;
 	benefit=(float)5/3040;
