fe8e2aab9962a0e0185e44c0a7c09b0f09fb0f57
andrew841018 <andrew841018@gmail.com>
2022-01-19 23:32:53 +0800

    solve bug

--- AI/duration_model.py
+++ AI/duration_model.py
@@ -34,16 +34,21 @@ x=[]##cached request
 zero=0
 one=0
 two=0
+special_use=[]
 for i in range(len(duration)):
-    if duration[i][1]<40:#duration<write buffer size=40 block
+    if duration[i][1]<40 and duration[i][0] not in special_use:#duration<write buffer size=40 block
         duration_label[int(duration[i][0])]=0#class 0=soon label
+        special_use.append(duration[i][0])
         zero+=1
-    if 40<duration[i][1] and duration[i][1]<5*40:
+    if 40<duration[i][1] and duration[i][1]<5*40 and duration[i][0] not in special_use:
         duration_label[int(duration[i][0])]=1#class 1=mean label
+        special_use.append(duration[i][0])
         one+=1
-    if 5*40<duration[i][1]: 
+    if 5*40<duration[i][1] and duration[i][0] not in special_use: 
         duration_label[int(duration[i][0])]=2#class 2=late label
+        special_use.append(duration[i][0])
         two+=1
+        
 duration_label=np.array(duration_label)
 c=0
 y=[]
@@ -65,14 +70,14 @@ y_test=np.array(y_test)
 x_train,x_test=train_test_split(x,random_state=777,train_size=0.8)
 #y_train=np_utils.to_categorical(y_train,3)
 y_train,y_test=train_test_split(y,random_state=777,train_size=0.8)
-for i in range(22):
+for i in range(6):
     x_train = np.delete(x_train,0, axis = 0)
     y_train = np.delete(y_train,0, axis = 0)
     x_test=np.delete(x_test,0,axis=0)
     y_test=np.delete(y_test,0, axis = 0)
 index=0
 for i in range(len(x_train)):
-    if (c+1) % 32!=0:
+    if (c+1) % 16!=0:
         y_train=np.delete(y_train,index,axis=0)
     else:
         index+=1#確定第31,63,95...比答案不會被刪除
@@ -80,13 +85,13 @@ for i in range(len(x_train)):
 c=0
 index=0
 for i in range(len(x_test)):
-    if (c+1) % 32!=0:
+    if (c+1) % 16!=0:
         y_test=np.delete(y_test,index,axis=0)
     else:
         index+=1#確定第31,63,95...比答案不會被刪除
     c+=1
-x_train=x_train.reshape(4638,32,7)
-x_test=x_test.reshape(1159,32,7)
+x_train=x_train.reshape(9277,16,7)
+x_test=x_test.reshape(2319,16,7)
 y_test=np_utils.to_categorical(y_test,3)
 y_train=np_utils.to_categorical(y_train,3)
 
@@ -101,7 +106,7 @@ metric=[
         ]
 model=Sequential()
 ##128=LSTM output size
-model.add(LSTM(128,input_shape=(32,7),activation='relu',return_sequences=True))
+model.add(LSTM(128,input_shape=(16,7),activation='relu',return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(128,activation='relu',return_sequences=True))
 model.add(Dropout(0.2))
@@ -122,7 +127,7 @@ input_shape format=(batch size,timestep,input dimension)
 PS:model.fit當中validation_data等同於evaluate功能，兩者選其一
 '''
 weight={0:1.005639647010695,1:214.201244813278,2:1064.381443298969}
-history=model.fit(x_train,y_train,epochs=3000,validation_data=(x_test,y_test))
+history=model.fit(x_train,y_train,epochs=3000,validation_data=(x_test,y_test),class_weight=weight)
 #注意，下面這個檔案會存在spyder當下所在，而非程式位置，可用cd更改位置
 '''
 plt.figure(dpi=250)#dpi越高，像素越高
