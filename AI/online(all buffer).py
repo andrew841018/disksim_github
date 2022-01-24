6b1767d9f8661136213b9fefdcd29df26c4f0fec
andrew841018 <andrew841018@gmail.com>
2022-01-24 23:29:59 +0800

    1/24 2318
    Add some code used to predict every request.
    The original LSTM will look at previous N requests information, which will be used to predict the current requests, ignoring the last request n requests.
    The code which I add will fix that problem.

--- AI/online(all buffer).py
+++ AI/online(all buffer).py
@@ -8,13 +8,20 @@ addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\p
 
 duration_model = tf.keras.models.load_model('(all buffer)duration_model.h5')
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\trace(for testing)\\"
-data=np.loadtxt(addr+'trace(for testing)\\info(trace_name).txt',delimiter=' ')
-tmp=[] 
-for i in range(len(data)):#initialization
-    tmp.append(data[i][31])
-while(len(tmp)!=32*len(data)):
-    for i in range(len(data)):#一個迴圈是將每批req,打亂一次
-        np.random.shuffle(data[i])#打亂順序，讓每個req都能被預測，否則LSTM架構會忽略參考的req            
-        while(str(data[i][31]) in tmp):
-            np.random.shuffle(data[i])
-        tmp.append(data[i][31])#紀錄要被預測的req
+data=np.loadtxt(addr+'trace(for testing)\\info(trace_name).txt',delimiter=' ',usecols=range(7))
+tmp=np.zeros(16,5)
+start=0
+finish=15
+c=0
+result=np.array([])
+while(finish!=len(data)):
+    for i in range(start,finish,1):
+        tmp[c]=test[i]
+        c+=1
+    c=0
+    tmp=tmp.reshape(1,16,5)
+    result=np.append(result,model.predict(tmp))
+    print(model.predict(tmp))
+    tmp=tmp.reshape(16,5)
+    start+=1
+    finish+=1
