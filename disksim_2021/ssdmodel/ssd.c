2a14adfe2ddceabe44891b4d5487f8d48fbadc91
andrew841018 <andrew841018@gmail.com>
2022-02-14 21:06:05 +0800

    use more...easy way?

--- disksim_2021/ssdmodel/ssd.c
+++ disksim_2021/ssdmodel/ssd.c
@@ -4616,7 +4616,7 @@ void mark_for_specific_current_block(buffer_cache *ptr_buffer_cache,unsigned int
     return;
   }
   
-  return;
+
 	//mark write intensive node
 	current_block[channel_num][plane].ptr_lru_node = ptr_buffer_cache->ptr_current_mark_node;
 	current_block[channel_num][plane].offset_in_node = ptr_buffer_cache->current_mark_offset;
@@ -5024,15 +5024,14 @@ void A_kick_page_from_buffer_cache(ioreq_event *curr,buffer_cache *ptr_buffer_ca
        //number of pages has been marked for read-intensive.
         statistic.kick_read_intensive_page_count ++;
         //踢掉read intensive page.....因為write buffer通常對於write page比較有利
-        write_buffer=ptr_buffer_cache;
 		    simulate(write_buffer);
-        kick_read_intensive_page_from_buffer_cache(curr,channel_num,plane,write_buffer);
+        kick_read_intensive_page_from_buffer_cache(curr,channel_num,plane,ptr_buffer_cache);
         current_block[channel_num][plane].flush_w_count_in_current ++;
       
         if(current_block[channel_num][plane].ptr_read_intensive_buffer_page  == NULL)
         {
           //mark read or write page
-          mark_for_specific_current_block(write_buffer,channel_num,plane);
+          mark_for_specific_current_block(ptr_buffer_cache,channel_num,plane);
         }
         
       }
@@ -5562,14 +5561,14 @@ void mark_for_read_intensive_buffer(buffer_cache *ptr_buffer_cache)
   }*/
   assert(ptr_buffer_cache->ptr_current_mark_node != ptr_buffer_cache->ptr_head);
 
-  lru_node *ptr_lru_node = ptr_buffer_cache->hash_Pg[hash_pg_index];
+  lru_node *ptr_lru_node = ptr_buffer_cache->ptr_current_mark_node;
   assert(ptr_lru_node->rw_intensive == 1);
-  //select_min_mark_current_block();  
+  select_min_mark_current_block();  
   for(i = 0;i < LRUSIZE;i++)
   {
     if(ptr_lru_node->page[i].exist == 1)
     {
-      //get_min_mark_plane(&channel_num,&plane);
+      get_min_mark_plane(&channel_num,&plane);
       ptr_lru_node->page[i].exist = 2;
       ptr_lru_node->page[i].channel_num = channel_num;
       ptr_lru_node->page[i].plane = plane;
