4bc8406407de4e1f210b0618ef90e5f59b9cfebd
andrew841018 <andrew841018@gmail.com>
2022-02-15 15:16:49 +0800

    2/15 1516

--- disksim_2021/ssdmodel/ssd.c
+++ disksim_2021/ssdmodel/ssd.c
@@ -4998,6 +4998,9 @@ void A_kick_page_from_buffer_cache(ioreq_event *curr,buffer_cache *ptr_buffer_ca
     //  plane = min_valid_page_in_plane(sta_die_num,currdisk,channel_num);
       //printf("ytc94u channel_num = %d plane = %d\n",channel_num,plane);
 
+
+    //表示要寫入SSD的channel,plane分別是：channel_num,plane，然後寫入的block是ptr_lru_node
+    //然後寫入的page index=offset_in_node. 
       ptr_lru_node = current_block[channel_num][plane].ptr_lru_node;
       offset_in_node = current_block[channel_num][plane].offset_in_node;
       
@@ -5081,8 +5084,8 @@ void A_kick_page_from_buffer_cache(ioreq_event *curr,buffer_cache *ptr_buffer_ca
 
         }
 
-		write_buffer=ptr_buffer_cache;
-		simulate(write_buffer);
+		    write_buffer=ptr_buffer_cache;
+		    simulate(write_buffer);
         remove_a_page_in_the_node(offset_in_node,ptr_lru_node,ptr_buffer_cache,channel_num,plane,flag);
         current_block[channel_num][plane].flush_w_count_in_current ++;
         //fprintf(lpb_ppn, "current_block[%d][%d].current_mark_count = %d\n", channel_num,plane,current_block[channel_num][plane].current_mark_count);
@@ -5690,7 +5693,7 @@ int remove_and_get_a_victim_logical_block(void) // decide which LB to be scatter
 {
     struct read_access_node *ptr_node;
     unsigned int logical_number = 0;
-    unsigned int max_read_count = 0;
+    unsigned int max_read_count = 0
     if(ptr_read_access_head_node == NULL)
         return -1;
     if(ptr_read_access_head_node->read_count < striping_threshold)
