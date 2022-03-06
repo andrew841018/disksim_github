e3872657f1845c83867e8ef4e47eeee80a1af67c
andrew841018 <andrew841018@gmail.com>
2022-03-06 21:21:00 +0800

    add some testing code

--- disksim_2021/ssdmodel/ssd.c
+++ disksim_2021/ssdmodel/ssd.c
@@ -5834,7 +5834,7 @@ void A_kick_page_from_buffer_cache(ioreq_event *curr,buffer_cache *ptr_buffer_ca
     if(current_block[channel_num][plane].ptr_lru_node->logical_node_num==65547){
       sleep(1);
     }
-    while(k<LRUSIZE && current_block[channel_num][plane].current_mark_count>0)
+    while(k<LRUSIZE && ptr_lru_node->buffer_page_num>0)
     {	  
       if(no_page_can_evict != 0)//預防機制，實際上不會進入這裡
       {
@@ -5942,19 +5942,19 @@ void A_kick_page_from_buffer_cache(ioreq_event *curr,buffer_cache *ptr_buffer_ca
 
         }		     
 		if(ptr_lru_node->page[k].exist == 2 ){
-			//printf("buffer num:%d\n",ptr_lru_node->buffer_page_num);
-      if(ptr_lru_node->buffer_page_num==1 && mark_bool[ptr_lru_node->logical_node_num]==1){
-          printf("remove block:%d k:%d mark count:%d\n",ptr_lru_node->logical_node_num,k,current_block[channel_num][plane].current_mark_count);		
-          mark_bool[ptr_lru_node->logical_node_num]=0;
-          }
-      else if(mark_bool[ptr_lru_node->logical_node_num]==0){
-        printf("kick page but not been marked...%d k:%d",ptr_lru_node->logical_node_num,k);
-        exit(0);
-      }
-      if(ptr_lru_node->buffer_page_num==1){
-        printf("mark_bool[%d]=%d\n",ptr_lru_node->logical_node_num,mark_bool[ptr_lru_node->logical_node_num]);
-      }
-      printf("remove page:%d\n",k);
+			printf("buffer num:%d\n",ptr_lru_node->buffer_page_num);
+			if(ptr_lru_node->buffer_page_num==1 && mark_bool[ptr_lru_node->logical_node_num]==1){
+			  printf("remove block:%d k:%d mark count:%d\n",ptr_lru_node->logical_node_num,k,current_block[channel_num][plane].current_mark_count);		
+			  mark_bool[ptr_lru_node->logical_node_num]=0;
+			  }
+			else if(mark_bool[ptr_lru_node->logical_node_num]==0){
+			printf("kick page but not been marked...%d k:%d",ptr_lru_node->logical_node_num,k);
+			exit(0);
+			}
+			if(ptr_lru_node->buffer_page_num==1){
+			printf("mark_bool[%d]=%d\n",ptr_lru_node->logical_node_num,mark_bool[ptr_lru_node->logical_node_num]);
+			}
+			printf("remove page:%d\n",k);
 
     	//when we remove all page in ptr_lru_node-->buffer page=0,it will free(ptr_lru_node).
 			//一旦執行free，代表整個block都會消失
