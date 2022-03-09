c156a46fe683f7f1bb3dec08a75bafb3fe5fb456
andrew841018 <andrew841018@gmail.com>
2022-03-09 01:32:49 -0800

    3/9 1731

--- 檔案系統/HW1_3.c
+++ 檔案系統/HW1_3.c
@@ -1,5 +1,7 @@
-#include <sys.mman.h>//mmap
+#include <sys/mman.h>//mmap
 #include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/stat.h>//open()
 #include <fcntl.h>
 #include <unistd.h>
@@ -8,13 +10,46 @@
 #include <string.h>//memcpy()
 #include <errno.h>
 int main(){
-    char buffer[];
+    struct timeval start;
+	struct timeval end;
+	unsigned long diff;
+    char buffer[4096];
+    int i,count=1024*1024*100/4096;
     char *map;//mapping variable
-    f1=open("file.txt","O_RDWR");
+    int  f1=open("file.txt",O_RDWR);
     if(f1==-1){
-        perror("error:");
+        perror("error:\n");
     }
     //get file size
+    struct stat st;
+    stat("file.txt",&st);
+    long int file_size=st.st_size;
+    //mmap
+    map=mmap(NULL,file_size,PROT_WRITE | PROT_READ,MAP_SHARED,f1,0);
+    if(map==MAP_FAILED){
+        perror("error\n");
+        return 0;
+    }
+    //sequential read
+    gettimeofday(&start,NULL);
+    for(i=0;i<count;i++){
+        memcpy(buffer,map,4096);//4kb
+        map+=4096;
+    }
+    gettimeofday(&end,NULL);
+	diff = 1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
+	printf("sequential Time: %ld us\n", diff);
+    map=0;
+    //sequential write
+    gettimeofday(&start,NULL);
+    for(i=0;i<count;i++){
+        memcpy(buffer,map,4096);//4kb
+        map+=4096;
+        fsync(f1);
+    }
+    gettimeofday(&end,NULL);
+	diff = 1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
+	printf("sequential Time: %ld us\n", diff);
 
 
 }
--- 檔案系統/creatfiles.c
+++ 檔案系統/creatfiles.c
@@ -8,12 +8,12 @@ int main ()
 {
 	FILE *fp;
 	int k,i;
-	for(k=0;k<10000000;k++)
+	/*for(k=0;k<10000000;k++)
 	{
 		strcat(wbuffer,"aaaaaaaaaa");//1 character = 1 byte.
-	}
-	fp = fopen("file.txt","a");
-	for(i=0;i<10000000;i++){
+	}*/
+	fp = fopen("file.txt","w");
+	for(i=0;i<1024*1024*10;i++){
 		fprintf(fp,"aaaaaaaaaa");
 	}
 	fclose(fp);
