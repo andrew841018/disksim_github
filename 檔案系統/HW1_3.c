b5690a5e95c248fdfcb358c72cb597a40c8e4d76
andrew841018 <andrew841018@gmail.com>
2022-03-09 01:51:39 -0800

    3/9 1750

--- 檔案系統/HW1_3.c
+++ 檔案系統/HW1_3.c
@@ -13,7 +13,7 @@ int main(){
     struct timeval start;
 	struct timeval end;
 	unsigned long diff;
-    char buffer[4096];
+    char buffer[4096],disk[4096];
     int i,count=1024*1024*100/4096;
     char *map;//mapping variable
     int  f1=open("file.txt",O_RDWR);
@@ -39,17 +39,25 @@ int main(){
     gettimeofday(&end,NULL);
 	diff = 1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
 	printf("sequential Time: %ld us\n", diff);
-    map=0;
     //sequential write
+    map=mmap(NULL,file_size,PROT_WRITE | PROT_READ,MAP_SHARED,f1,0);
     gettimeofday(&start,NULL);
-    for(i=0;i<count;i++){
-        memcpy(buffer,map,4096);//4kb
-        map+=4096;
+    for(i=0;i<1024;i++){
+        strcat(disk,"ar");
+    }
+    
+    for(i=0;i<count*2;i++){
+        memcpy(map,disk,2048);//4kb
+        map+=2048;
         fsync(f1);
     }
     gettimeofday(&end,NULL);
 	diff = 1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
 	printf("sequential Time: %ld us\n", diff);
+    //random read
+    for(i=0;i<50000;i++){
+        
+    }
 
-
+    return 0;
 }
