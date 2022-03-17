51a8a929667b4c3c80c8fce84fd52c4771a98496
andrew841018 <andrew841018@gmail.com>
2022-03-17 11:49:22 +0800

    set

--- disksim_new_method/disksim_new_method/ssdmodel/ssd_gang.h
+++ disksim_new_method/disksim_new_method/ssdmodel/ssd_gang.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // DiskSim SSD support
 // ©2008 Microsoft Corporation. All Rights Reserved
 
@@ -14,3 +15,21 @@ void ssd_access_complete_gang_sync(ioreq_event *curr);
 void ssd_access_complete_gang(ioreq_event *curr);
 
 #endif
+=======
+// DiskSim SSD support
+// ©2008 Microsoft Corporation. All Rights Reserved
+
+#ifndef _DISKSIM_SSD_GANG_H
+#define _DISKSIM_SSD_GANG_H
+
+#include "ssd.h"
+#include "modules/ssdmodel_ssd_param.h"
+
+void ssd_media_access_request_gang_sync(ioreq_event *curr);
+void ssd_media_access_request_gang(ioreq_event *curr);
+void ssd_clean_gang_complete(ioreq_event *curr);
+void ssd_access_complete_gang_sync(ioreq_event *curr);
+void ssd_access_complete_gang(ioreq_event *curr);
+
+#endif
+>>>>>>> d0beeb997e5b47bb468c47d1ed3571fdbfe40eff
