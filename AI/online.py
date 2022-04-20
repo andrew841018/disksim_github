f843840be2b63a4574dcd438378f2fabea3c45bd
andrew841018 <andrew841018@gmail.com>
2022-04-20 10:46:08 +0800

    update python code

--- AI/online.py
+++ AI/online.py
@@ -4,10 +4,10 @@ from pandas import DataFrame
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\physical\\"
-duration_model = tf.keras.models.load_model(addr+'(with ignore)duration_model.h5')
-addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\trace(for testing)\\"
-data=np.loadtxt(addr+'info(iozone2).txt',delimiter=' ',usecols=range(7))
+addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\"
+duration_model = tf.keras.models.load_model(addr+'duration_model.h5')
+addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\trace(used to build RNN)\\"
+data=np.loadtxt(addr+'info(run1_Postmark_2475).txt',delimiter=' ',usecols=range(7))
 '''
 tmp=np.array([])
 for i in range(16):
@@ -31,44 +31,12 @@ while(finish!=len(data)):
         output=1
     if duration_model.predict(tmp)[0][2]==np.max(duration_model.predict(tmp)):
         output=2   
-    result=np.append(result,output)
-    print(output)
+    with open('online(run1_Postmark_2475).txt','a') as f:
+        f.write(str(output)+"\n")
     #print(output)
     tmp=tmp.reshape(16,7)
     start+=1
     finish+=1
-np.savetxt('iozone2_duration_info.txt',result)
-hit_count=0
-total=0
-for i in open('iozone2_duration_info.txt').readlines():
-    for j in open('duration.txt').readlines():
-        if int(j.split()[1])<2560:
-            label=0
-            hit_count+=1
-        elif int(j.split()[1])<2560*5:
-            label=1
-        if int(j.split()[1])>=5*2560:
-            label=2            
-        total+=1
-        break
-hit_count=0
-total=0
-zero=0
-one=0
-two=0
-for i in open('duration.txt').readlines():
-    if int(i.split()[1])<2560:
-        label=0
-        zero+=1
-        hit_count+=1
-    elif int(i.split()[1])<2560*5:
-        label=1
-        one+=1
-    if int(i.split()[1])>=5*2560:
-        label=2
-        two+=1            
-    total+=1
-    print('{:.10%}'.format(hit_count/total))
 
 
 
