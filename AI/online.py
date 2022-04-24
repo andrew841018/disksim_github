3a046764509fab2c768843fc1a84be45568728c2
andrew841018 <andrew841018@gmail.com>
2022-04-24 11:03:41 +0800

    make some change for input data
    將block number拿去辨識當下duration label是屬於哪個block
    但實際做訓練時，會將block number整列拿掉，避免誤導機器

--- AI/online.py
+++ AI/online.py
@@ -8,12 +8,15 @@ addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\"
 duration_model = tf.keras.models.load_model(addr+'duration_model.h5')
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\trace(used to build RNN)\\"
 data=np.loadtxt(addr+'info(run1_Postmark_2475).txt',delimiter=' ',usecols=range(7))
+data_for_predict=data
+data_for_predict=np.delete(data_for_predict,2,axis=1)#delete physical_block_number
+
 '''
 tmp=np.array([])
 for i in range(16):
     tmp=np.append(tmp,0)
 '''
-tmp=np.zeros((16,7))
+tmp=np.zeros((16,6))
 start=0
 finish=15
 c=0
@@ -21,10 +24,10 @@ output=""
 result=np.array([])
 while(finish!=len(data)):
     for i in range(start,finish,1):
-        tmp[c]=data[i]
+        tmp[c]=data_for_predict[i]
         c+=1
     c=0
-    tmp=tmp.reshape(1,16,7)
+    tmp=tmp.reshape(1,16,6)
     if duration_model.predict(tmp)[0][0]==np.max(duration_model.predict(tmp)):
         output=0
     if duration_model.predict(tmp)[0][1]==np.max(duration_model.predict(tmp)):
@@ -34,7 +37,7 @@ while(finish!=len(data)):
     with open('online(run1_Postmark_2475).txt','a') as f:
         f.write(str(data[finish][2])+" "+str(output)+"\n")
     #print(output)
-    tmp=tmp.reshape(16,7)
+    tmp=tmp.reshape(16,6)
     start+=1
     finish+=1
 
