342c30c5846dd587f53b446132401bc07689f7b2
andrew841018 <andrew841018@gmail.com>
2022-05-14 15:52:28 +0800

    add blas

--- AI/duration_model.py
+++ AI/duration_model.py
@@ -134,14 +134,14 @@ metric=[
         ]
 model=Sequential()
 ##128=LSTM output size
-model.add(LSTM(128,input_shape=(16,6),activation='relu',return_sequences=True))
+model.add(LSTM(128,input_shape=(16,6),activation='sigmoid',return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(128,activation='relu',return_sequences=True))
+model.add(LSTM(128,activation='sigmoid',return_sequences=True))
 model.add(Dropout(0.2))
 
 #return_sequences=True.....將所有time step output 輸出
 #false.....只輸出最後一個time step output
-model.add(LSTM(128,activation='relu'))
+model.add(LSTM(128,activation='sigmoid'))
 model.add(Dropout(0.2))
 model.add(Dense(3,activation='softmax'))#classify into 1 class
 
