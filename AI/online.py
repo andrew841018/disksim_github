17c61691311bc56227e674f80a2f6ad0e2097132
andrew841018 <andrew841018@gmail.com>
2022-05-16 09:40:28 +0800

    add new model(iozone)

--- AI/online.py
+++ AI/online.py
@@ -4,10 +4,10 @@ from pandas import DataFrame
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\run1_Postmark_2475\\"
-duration_model = tf.keras.models.load_model(addr+'duration_model(sigmoid-780 epoch).h5')
+addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\"
+duration_model = tf.keras.models.load_model(addr+'duration_model(iozone).h5')
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\AI input feature\\"
-data=np.loadtxt(addr+'info(run1_Postmark_2475).txt',delimiter=' ',usecols=range(7))
+data=np.loadtxt(addr+'info(iozone).txt',delimiter=' ',usecols=range(7))
 data_for_predict=data
 data_for_predict=np.delete(data_for_predict,2,axis=1)#delete physical_block_number
 
@@ -30,7 +30,7 @@ while(finish!=len(data)):
         output=1
     if duration_model.predict(tmp)[0][2]==np.max(duration_model.predict(tmp)):
         output=2   
-    with open('online(run1_Postmark_2475)_duration.txt','a') as f:
+    with open('online(iozone)_duration.txt','a') as f:
         f.write(str(data[finish][2])+" "+str(output)+"\n")
     #print(output)
     tmp=tmp.reshape(16,6)
