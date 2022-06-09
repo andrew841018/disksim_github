9f0aadc51d85d933e2bb0aff6db17a696434bcf5
andrew841018 <andrew841018@gmail.com>
2022-06-10 00:13:01 +0800

    6/10 0012

--- AI/online.py
+++ AI/online.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\"
 duration_model = tf.keras.models.load_model(addr+'duration_model(run1_Postmark_2475).h5')
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\AI input feature\\generate by current code\\"
-data=np.loadtxt(addr+'info(run1_Postmark_2475).txt',delimiter=' ',usecols=range(7))
+data=np.loadtxt(addr+'info(wr_run1_ug3).txt',delimiter=' ',usecols=range(7))
 data_for_predict=data
 data_for_predict=np.delete(data_for_predict,2,axis=1)#delete physical_block_number
 
@@ -35,7 +35,7 @@ while(finish!=len(data)):
         output=1
     if duration_model.predict(tmp)[0][2]==np.max(duration_model.predict(tmp)):
         output=2   
-    with open('duration(run1_Postmark_2475).txt','a') as f:
+    with open('duration(wr_run1_ug3).txt','a') as f:
         f.write(str(data[finish][2])+" "+str(output)+"\n")
     content.append(data[finish][2])
     #print(output)
