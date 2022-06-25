1be413c2736f343c1bf2daf822c41fc1be65627e
andrew841018 <andrew841018@gmail.com>
2022-06-25 17:37:31 +0800

    6/25 1735

--- AI+hint(use multiply method)/disksim_new_method/ssdmodel/ssd.c
+++ AI+hint(use multiply method)/disksim_new_method/ssdmodel/ssd.c
@@ -4268,7 +4268,7 @@ int Y_add_Pg_page_to_cache_buffer(unsigned int lpn,buffer_cache *ptr_buffer_cach
 	}
   	//arrive time,read count,physical_node_num,write count,block size,block_write_count,page_write_count
 	if(ptr_buffer_cache->max_buffer_page_num==4000){
-		FILE *t=fopen("info(run1_asim_ug2).txt","a+");
+		FILE *t=fopen("info(iozone2).txt","a+");
 		fprintf(t,"%f %d %d %d %d %d %d\n",curr1->arrive_time,LPN_RWtimes[physical_node_num][0],physical_node_num,LPN_RWtimes[physical_node_num][1],ptr_buffer_cache->hash_Pg[physical_node_num % HASHSIZE]->block_size,block_write_count,write_count[physical_node_num][phy_node_offset]);
 		fclose(t);
 	}
