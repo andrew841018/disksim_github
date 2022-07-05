ad4f2448ef52ff7db3145d67fa06473e56dd059d
andrew841018 <andrew841018@gmail.com>
2022-07-05 17:04:45 +0800

    7/5 1704

--- AI/online.py
+++ AI/online.py
@@ -3,11 +3,13 @@ import random
 from pandas import DataFrame
 import numpy as np
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
+start=time.perf_counter()
 addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\AI\\model\\"
-duration_model = tf.keras.models.load_model(addr+'duration_model(iozone2).h5')
-addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\AI input feature\\generate by current code\\AI+Hint\\"
-data=np.loadtxt(addr+'info(iozone2).txt',delimiter=' ',usecols=range(7))
+duration_model = tf.keras.models.load_model(addr+'duration_model(run1_Postmark_2475).h5')
+addr="C:\\Users\\user\\Dropbox\\shared with ubuntu\\disksim_github\\collected data(from disksim)\\AI input feature\\generate by current code\\AI\\"
+data=np.loadtxt(addr+'info(run1_Postmark_2475).txt',delimiter=' ',usecols=range(7))
 data_for_predict=data
 data_for_predict=np.delete(data_for_predict,2,axis=1)#delete physical_block_number
 
@@ -35,13 +37,16 @@ while(finish!=len(data)):
         output=1
     if duration_model.predict(tmp)[0][2]==np.max(duration_model.predict(tmp)):
         output=2   
-    with open('duration(iozone2).txt','a') as f:
-        f.write(str(data[finish][2])+" "+str(output)+"\n")
+    #with open('duration(run1_Postmark_2475).txt','a') as f:
+        #f.write(str(data[finish][2])+" "+str(output)+"\n")
     content.append(data[finish][2])
     #print(output)
     tmp=tmp.reshape(16,6)
     start+=1
     finish+=1
+end=time.perf_counter()
+avg=(end-start)/len(data)
+print("avg request response time:",avg)
 '''*** buffer part***
 tmp=np.zeros((16,6))
 start=0
